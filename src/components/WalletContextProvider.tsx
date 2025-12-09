f7a73b15751f390f6849da0b9ff98953b3cee885
ErenIsFree <2824889114@qq.com>
2025-12-10 04:28:49 +0800

    Update WalletContextProvider.tsx

--- src/components/WalletContextProvider.tsx
+++ src/components/WalletContextProvider.tsx
@@ -4,25 +4,17 @@ import { FC, ReactNode, useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
-import { toast } from "react-hot-toast";
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const endpoint = "https://mainnet.helius-rpc.com/?api-key=f6ac37ee-435b-440c-9114-87bf7783319b";
   const wallets = useMemo(() => [], []);
 
-return (
-    <ConnectionProvider endpoint={endpoint} config={{ commitment: 'confirmed' }}>
-      <WalletProvider 
-        wallets={wallets} 
-        autoConnect
-        onError={(error) => {
-            console.error("钱包错误:", error);
-            toast.error(`连接错误: ${error.name} - ${error.message}`, {
-                style: { background: '#333', color: '#fff', wordBreak: 'break-all' },
-                duration: 5000 
-            });
-        }}
-      >
+  return (
+    <ConnectionProvider 
+        endpoint={endpoint}
+        config={{ commitment: 'confirmed' }}
+    >
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
