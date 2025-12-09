b896634691a4af3c3a6d3664bc1cf846ddd3b050
ErenIsFree <2824889114@qq.com>
2025-12-10 00:03:40 +0800

    Update WalletContextProvider.tsx

--- src/components/WalletContextProvider.tsx
+++ src/components/WalletContextProvider.tsx
@@ -3,28 +3,12 @@
 import { FC, ReactNode, useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { 
-    PhantomWalletAdapter,
-    SolflareWalletAdapter,
-    TorusWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
-
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const endpoint = useMemo(() => 
-    "https://mainnet.helius-rpc.com/?api-key=ee54db44-f348-4700-b17d-1bc7f33a605b", 
-  []);
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new TorusWalletAdapter(),
-    ],
-    []
-  );
+  const endpoint = "https://mainnet.helius-rpc.com/?api-key=ee54db44-f348-4700-b17d-1bc7f33a605b";
+  const wallets = useMemo(() => [], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
