369b446d04c4aad7049706ceb0962c0c90826e4a
ErenIsFree <2824889114@qq.com>
2025-12-17 22:48:21 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 1. 核心：导出为纯静态文件 (配合 Cloudflare Pages)
-  output: 'export',
+    output: 'export',
 
-  // 2. 关闭图片优化 (静态导出必须关)
-  images: {
+    images: {
     unoptimized: true,
-  },
+    },
 
-  // 3. 忽略类型检查 (强制过关)
-  typescript: {
+    typescript: {
     ignoreBuildErrors: true,
-  },
-  // 注意：eslint 配置项在 Next.js 新版中不支持放在这里，所以我删掉了
+    },
+
+    eslint: {
+    ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
