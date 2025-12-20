7212f73358a08b5e8922b9ab348cfd44f739d741
ErenIsFree <2824889114@qq.com>
2025-12-21 03:15:30 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { UnifiedWalletButton } from "@jup-ag/wallet-adapter";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useEffect, useState, useRef, useCallback } from "react";
 import { motion, AnimatePresence } from "framer-motion";
@@ -224,24 +224,7 @@ const Navbar = ({
 
         <div className="flex items-center gap-2">
           <div id="mini-wallet-wrapper" className="origin-right relative">
-            <WalletMultiButton style={{ padding: 0, minWidth: 0 }}>
-                <div className="relative flex items-center justify-center w-full h-full">
-                    {connected && wallet ? (
-                        <img 
-                            src={wallet.adapter.icon} 
-                            alt={wallet.adapter.name} 
-                            className="w-6 h-6 rounded-full object-cover custom-wallet-logo" 
-                        />
-                    ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-gray-300">
-                          <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
-                        </svg>
-                    )}
-                    {connected && (
-                        <span className="absolute top-[-2px] right-[-2px] w-2.5 h-2.5 bg-green-500 border-2 border-gray-900 rounded-full z-10"></span>
-                    )}
-                </div>
-            </WalletMultiButton>
+            <UnifiedWalletButton />
           </div>
 
           <div className="relative">
